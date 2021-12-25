c0a6bacd85da2b3d7281d8ff3e6c12eb048b7d20
GannaRF <gannahinggil@refactory.id>
2021-12-25 18:22:17 +0700

    add footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      2021 COPYRIGHT @SOLANADOODLEPUNKS.IO
+    </div>
+  )
+}
+
+export default Footer
