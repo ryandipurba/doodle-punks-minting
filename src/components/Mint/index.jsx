e3cf411bcd65644d917809784a929dee10f9a83d
GannaRF <gannahinggil@refactory.id>
2021-12-25 18:21:27 +0700

    Add Mint Section

---
+++ src/components/Mint/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Giphy from '../../assets/giphy.gif'
+import App from '../../App'
+import './index.css'
+
+const MintSection = () => {
+  return (
+    <div className="mint">
+      <img src={Giphy} alt="" className="gif-doodles" />
+      <h2>Mint a Doodle punks</h2>
+      <span className="qty">0.12 SOL</span>
+      <App />
+    </div>
+  )
+}
+
+export default MintSection
