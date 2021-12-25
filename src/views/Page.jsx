ecd347dec5ea4da9b26aca2429ccb4b7ec27b592
GannaRF <gannahinggil@refactory.id>
2021-12-25 22:42:26 +0700

    add effect for Roadmap

--- src/views/Page.jsx
+++ src/views/Page.jsx
@@ -3,8 +3,10 @@ import Navbar from '../components/Navbar'
 import MintSection from '../components/Mint'
 import TimeLine from '../components/TimeLine'
 import Footer from '../components/Footer'
+import { reveal } from "../utils/reveal"
 
 const Page = () => {
+  window.addEventListener('scroll', reveal)
   return (
     <div>
       <Navbar />
