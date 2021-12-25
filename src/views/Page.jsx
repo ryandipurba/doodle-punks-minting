7624d991808f77190913b5648d2a76466e29ad2b
GannaRF <gannahinggil@refactory.id>
2021-12-25 18:22:46 +0700

    add screen page

---
+++ src/views/Page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import MintSection from '../components/Mint'
+import TimeLine from '../components/TimeLine'
+import Footer from '../components/Footer'
+
+const Page = () => {
+  return (
+    <div>
+      <Navbar />
+      <MintSection />
+      <TimeLine />
+      <Footer />
+    </div>
+  )
+}
+
+export default Page
