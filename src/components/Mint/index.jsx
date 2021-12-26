49da4ee803e017413fecdf4dae9dbdbe225f79e1
ryandi <purbaryandi@gmail.com>
2021-12-26 12:29:17 +0700

    update mint section

--- src/components/Mint/index.jsx
+++ src/components/Mint/index.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Giphy from '../../assets/giphy.gif'
-import App from '../../App'
-import './index.css'
+import React from "react";
+import Giphy from "../../assets/giphy.gif";
+import App from "../../App";
+import "./index.css";
 
 const MintSection = () => {
   return (
     <div className="mint">
       <img src={Giphy} alt="" className="gif-doodles" />
-      <h2>Mint a Doodle punks</h2>
+      <h2>Mint A Doodle Punk</h2>
       <span className="qty">Price 0.12 SOL</span>
       <App />
     </div>
-  )
-}
+  );
+};
 
-export default MintSection
+export default MintSection;
